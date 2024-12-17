fe2aa9d611b1a9f11f2eb491dd0577b9259fd57d
Navneeth <navneid997@gmail.com>
2024-12-18 00:30:48 +0530

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route
 } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
-      <Route path = "/Navaneeth_TP_Portfolio" element = {<App />}></Route>
+      <Route path="/My-Portfolio" element={<App />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
-
