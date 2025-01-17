d3c348942743cb971b9e47b0760c2e4e5bf6712d
Navneeth <navneid997@gmail.com>
2025-01-17 21:21:16 +0530

    Updated homepage for Netlify deployment

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
     <Routes>
-      <Route path="/My-Portfolio" element={<App />} />
+      <Route path="/" element={<App />} />
     </Routes>
   </HashRouter>
 );
