571a4b44bd0045c0f17d4a12c48b8fc4262b1127
Navneeth <navneid997@gmail.com>
2025-01-17 22:04:56 +0530

    Update header again

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className='container flex'>
             <div className='header-content'>
                 <h2 className='text-uppercase text-white op-07 fw-6 ls-2'>Frontend Dev, Backend Dev, Software Tester</h2>
-                <h1 className='text-white fw-6 header-title'>Hello, I'm a<span className='text-brown'> Full Stack Developer</span> From Indiaa.</h1>
+                <h1 className='text-white fw-6 header-title'>Hello, I'm a<span className='text-brown'> Full Stack Developer</span> From India.</h1>
                 <div className='btn-groups flex'>
                   <a  className='btn-item bg-brown fw-4 ls-2' href="https://github.com/Navneeth997">See Works</a>
                   <a  className='btn-item bg-dark fw-4 ls-2' href="mailto:navneid997@gmail.com">Contact mail</a>
